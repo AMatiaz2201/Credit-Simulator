6af03f620800e9896f52ecd022f2a67bf7f1a696
Abner Matias <abnersmatias@gmail.com>
2024-12-30 21:49:22 -0300

    Ajuste de versões no POM e criação do application.yml

--- src/main/java/com/example/credit/simulator/Application.java
+++ src/main/java/com/example/credit/simulator/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CreditSimulatorApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreditSimulatorApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
